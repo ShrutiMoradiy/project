240c82cf455a1645926fa97c1b6f9d3ace5f0f6d
shruti <shrutimoradiya1@gmail.com>
2024-04-08 18:46:29 +0530

    add changes

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -14,15 +14,24 @@ function Header() {
   const [showDropdown, setShowDropdown] = useState(false);
   const toggleMenu = () => {
     setShowNavbar(!showNavbar);
-    showToggle(true)
+  };
+
+  const handleMouseEnter = () => {
+    setShowDropdown(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowDropdown(false);
   };
 
   return (
     <>
-      <header className="w-full bg-slate-200">
+      <header className="w-full bg-[#f7f7f9]">
         <div className="container flex flex-wrap justify-around p-5 flex-col md:flex-row items-center">
           <div
-            className={`flex flex-row flex-grow text-center justify-between ${showToggle ? 'w-full' : 'w-auto'}`}
+            className={`flex flex-row flex-grow text-center justify-between ${
+              showToggle ? "w-full" : "w-full"
+            }`}
           >
             <Link href="/">
               <Image src={logo} width={119} height={30}></Image>
@@ -38,51 +47,59 @@ function Header() {
             >
               <Image src={menutoggle} width={20} height={20}></Image>
             </button>
-          </div>
-          <nav
-            className={`md:flex flex-grow items-center justify-end space-x-4 ${
-              showNavbar
-                ? "fixed top-0 left-0 h-full bg-white p-3 w-64 transform transition-transform duration-300"
-                : "hidden "
-            }`}
-          >
-            <div className={showNavbar ? "grid items-center" : "flex"}>
-              <Link href="/" className="px-3 flex justify-between">
-                Home
-                {showNavbar && (
-                  <span onClick={toggleMenu}>
-                    <Image src={close} width={24} height={24}></Image>
+            <nav
+              className={`md:flex flex-grow items-center justify-end space-x-4 ${
+                showNavbar
+                  ? "fixed top-0 left-0 h-full bg-white p-3 w-64 transform transition-transform duration-300"
+                  : "hidden "
+              }`}
+            >
+              <div className={showNavbar ? "grid items-center" : "flex"}>
+                <Link href="/" className="px-3 flex justify-between">
+                  Home
+                  {showNavbar && (
+                    <span onClick={toggleMenu}>
+                      <Image src={close} width={24} height={24}></Image>
+                    </span>
+                  )}
+                </Link>
+                <Link href="/" className={showNavbar ? "p-3 flex" : "px-3"}>
+                  Landings
+                </Link>
+                <Link href="/" className={showNavbar ? "p-3 flex" : "px-3"}>
+                  pages
+                </Link>
+                <div
+                  className="relative inline-flex items-center px-3"
+                  onMouseEnter={() => setShowDropdown(true)}
+                  onMouseLeave={() => setShowDropdown(false)}
+                >
+                  <span>Docs</span>
+                  <span className={showNavbar ? "p-3" : "px-3"}>
+                    <Image src={arrowdown} width={20} height={20}></Image>
                   </span>
-                )}
-              </Link>
-              <Link href="/" className={showNavbar ? "p-3" : "px-3"}>
-                Landings
-              </Link>
-              <Link href="/" className={showNavbar ? "p-3" : "px-3"}>
-                pages
-              </Link>
-              <div
-                className="relative inline-flex items-center px-3"
-                onMouseEnter={() => setShowDropdown(true)}
-                onMouseLeave={() => setShowDropdown(false)}
-              >
-                <span>Docs</span>
-                <span className={showNavbar ? "p-3" : "px-3"}>
-                  <Image src={arrowdown} width={20} height={20}></Image>
-                </span>
 
-                {showDropdown && (
-                  <div className={showNavbar ? "absolute grid mt-[80px] p-3" : "absolute bg-white grid shadow-md p-2 min-w-[200px] mt-28"}>
-                    <Link href="/">Getting Started</Link>
-                    <Link href="/">Components</Link>
-                  </div>
-                )}
+                  {showDropdown && (
+                    <div
+                      className={
+                        showNavbar
+                          ? "absolute grid mt-[80px] p-3 justify-start"
+                          : "absolute bg-[#f7f7f9] flex flex-col rounded shadow-md min-w-[150px] mt-28"
+                      }
+                    >
+                      <Link href="/" className="flex p-2">Getting Started</Link>
+                      <Link href="/" className="flex p-2">Components</Link>
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className={showNavbar && showDropdown ? "mt-16" : "justify-end"}>
-              <Link href="/">Log In</Link>
-            </div>
-          </nav>
+              <div
+                className={showNavbar && showDropdown ? "mt-16 flex" : "flex"}
+              >
+                <Link href="/">Log In</Link>
+              </div>
+            </nav>
+          </div>
         </div>
       </header>
     </>
